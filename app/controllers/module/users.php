5056204939d2b1df1e8643d9400d9a7b88750be8
KeruyCRM <keruy@keruy.com.ua>
2022-07-05 15:42:02 +0300

    F3

--- app/controllers/module/users.php
+++ app/controllers/module/users.php
@@ -14,10 +14,10 @@ class Users extends \Controller
         }
 
         //check security settings if they are enabled
-        app_restricted_countries::verify();
-        app_restricted_ip::verify();
+        \Helpers\App_restricted_countries::verify();
+        \Helpers\App_restricted_ip::verify();
 
-        if (app_session_is_registered('app_logged_users_id')) {
+        if (\K::app_session_is_registered('app_logged_users_id')) {
             $app_module_action = 'logoff';
         }
 
@@ -26,5 +26,37 @@ class Users extends \Controller
 
     public function login()
     {
+        if (\K::$fw->CFG_ENABLE_SOCIAL_LOGIN == 2) {
+            \Helpers\Urls::redirect_to('module/users/login');
+        }
+
+        //chck form token
+        //app_check_form_token('users/login');
+
+        //check reaptcha
+        if (app_recaptcha::is_enabled()) {
+            if (!app_recaptcha::verify()) {
+                $alerts->add(TEXT_RECAPTCHA_VERIFY_ROBOT, 'error');
+                redirect_to('users/login');
+            }
+        }
+
+        users::login($_POST['username'], $_POST['password'], (isset($_POST['remember_me']) ? 1 : 0));
+    }
+
+    public function logoff()
+    {
+        \K::app_session_unregister('app_logged_users_id');
+        \K::app_session_unregister('app_current_version');
+        \K::app_session_unregister('two_step_verification_info');
+        \K::app_session_unregister('app_email_verification_code');
+        \K::app_session_unregister('app_session_token');
+
+        \K::cookieClear('app_stay_logged');
+        \K::cookieClear('app_remember_user');
+        \K::cookieClear('app_remember_pass');
+        \K::cookieClear('izoColorPickerColors');
+
+        redirect_to('users/login');
     }
 }
---
+++ app/helpers/app_recaptcha.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Helpers;
+
+class App_recaptcha
+{
+    static function is_enabled()
+    {
+        if (strlen(\K::$fw->CFG_RECAPTCHA_KEY) and strlen(
+                \K::$fw->CFG_RECAPTCHA_SECRET_KEY
+            ) and \K::$fw->CFG_RECAPTCHA_ENABLE == true) {
+            if (!defined('CFG_RECAPTCHA_TRUSTED_IP')) {
+                define('CFG_RECAPTCHA_TRUSTED_IP', '');
+            }
+
+            if (strlen(\K::$fw->CFG_RECAPTCHA_TRUSTED_IP) and in_array(
+                    $_SERVER['REMOTE_ADDR'],
+                    array_map('trim', explode(',', \K::$fw->CFG_RECAPTCHA_TRUSTED_IP))
+                )) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    static function render_js()
+    {
+        if (self::is_enabled()) {
+            return '<script src="https://www.google.com/recaptcha/api.js?hl=' . \K::$fw->TEXT_APP_LANGUAGE_SHORT_CODE . '"></script>';
+        } else {
+            return '';
+        }
+    }
+
+    static function render()
+    {
+        return '<div class="g-recaptcha" data-sitekey="' . \K::$fw->CFG_RECAPTCHA_KEY . '"></div>';
+    }
+
+    static function verify()
+    {
+        require('app/libs/ReCaptcha/ReCaptcha.php');
+        require('app/libs/ReCaptcha/RequestMethod.php');
+        require('app/libs/ReCaptcha/RequestParameters.php');
+        require('app/libs/ReCaptcha/Response.php');
+        require('app/libs/ReCaptcha/RequestMethod/Curl.php');
+        require('app/libs/ReCaptcha/RequestMethod/CurlPost.php');
+
+        $recaptcha = new \ReCaptcha\ReCaptcha(\K::$fw->CFG_RECAPTCHA_SECRET_KEY);
+        $resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+
+        return $resp->isSuccess();
+    }
+}
