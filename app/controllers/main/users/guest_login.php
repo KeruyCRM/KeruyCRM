9d6ac9c9ffd2f9313fd5ea030eac3c91596d3885
KeruyCRM <keruy@keruy.com.ua>
2022-07-07 21:16:35 +0300

    guest_login.php

---
+++ app/controllers/main/users/guest_login.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Controllers\Main\Users;
+
+class Guest_login extends \Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        //check security settings if they are enabled
+        \Helpers\App_restricted_countries::verify();
+        \Helpers\App_restricted_ip::verify();
+    }
+
+    public function index()
+    {
+        if (\Models\Main\Users\Guest_login::is_enabled()) {
+            \K::app_session_register('app_logged_users_id', \K::$fw->CFG_GUEST_LOGIN_USER);
+
+            \Helpers\Urls::redirect_to('main/dashboard/dashboard');
+        } else {
+            \Helpers\Urls::redirect_to('main/users/login');
+        }
+    }
+}
