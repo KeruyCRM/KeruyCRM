c9e7218b2e5a7c3f7852dc29deb3df13d58542ba
KeruyCRM <keruy@keruy.com.ua>
2022-07-20 21:15:26 +0300

    change_skin.php

--- app/controllers/main/users/change_skin.php
+++ app/controllers/main/users/change_skin.php
@@ -4,7 +4,7 @@
  * https://keruy.com.ua
  */
 
-namespace Controllers\Main\Configuration;
+namespace Controllers\Main\Users;
 
 class Change_skin extends \Controller
 {
@@ -23,16 +23,25 @@ class Change_skin extends \Controller
         echo \K::view()->render(\K::$fw->subTemplate);
     }
 
-    public function change_skin(){
-        $skin = \K::$fw->GET['set_skin'];
+    public function change_skin()
+    {
+        if (isset(\K::$fw->GET['set_skin']) and \K::security()->checkCsrfTokenUrl()) {
+            $skin = \K::$fw->GET['set_skin'];
 
-        if (is_file('css/skins/' . $skin . '/' . $skin . '.css')) {
-            db_query("update app_entity_1 set field_14='" . db_input($skin) . "' where id='" . \K::$fw->app_logged_users_id . "'");
+            if (is_file('css/skins/' . $skin . '/' . $skin . '.css')) {
+                //db_query("update app_entity_1 set field_14='" . db_input($skin) . "' where id='" . \K::$fw->app_logged_users_id . "'");
 
-            //setcookie('user_skin', $skin, time() + (365 * 24 * 3600), $_SERVER['HTTP_HOST'], '', (is_ssl() ? 1 : 0));
-            \K::cookieSet('user_skin',$skin,time() + (365 * 24 * 3600));
+                \K::model()->db_update('app_entity_1', [
+                    'field_14' => $skin
+                ], [
+                    'id = ?',
+                    \K::$fw->app_logged_users_id
+                ]);
 
-            \Helpers\Urls::redirect_to('main/dashboard');
+                //setcookie('user_skin', $skin, time() + (365 * 24 * 3600), $_SERVER['HTTP_HOST'], '', (is_ssl() ? 1 : 0));
+                \K::cookieSet('user_skin', $skin, 60 * 60 * 24 * 36);
+            }
         }
+        \Helpers\Urls::redirect_to('main/dashboard');
     }
 }
--- app/views/main/users/change_skin.php
+++ app/views/main/users/change_skin.php
@@ -3,21 +3,22 @@
 if (!defined('KERUY_CRM')) {
     exit;
 } ?>
-<?php
-echo ajax_modal_template_header(TEXT_CHANGE_SKIN) ?>
+<?= \Helpers\App::ajax_modal_template_header(\K::$fw->TEXT_CHANGE_SKIN) ?>
 
     <div class="skins-list">
         <ul>
             <?php
-            foreach (app_get_skins_choices(false) as $skin => $name): ?>
+            $skins = \Helpers\App::app_get_skins_choices(false);
+            foreach ($skins as $skin => $name): ?>
                 <li>
-                    <?php
-                    echo $name; ?>
+                    <?= $name; ?>
                     <div style="border: 1px solid #b9b9b9; margin: 5px; width: 80px; height: 80px; cursor: pointer; background: white;"
-                         onClick="location='<?php
-                         echo url_for('users/change_skin', 'action=change_skin&set_skin=' . $skin); ?>'">
-                        <?php
-                        echo image_tag('css/skins/' . $skin . '/' . $skin . '.png'); ?>
+                         onClick="location='<?= \Helpers\Urls::url_for(
+                             'main/users/change_skin/change_skin',
+                             'set_skin=' . $skin,
+                             true
+                         ); ?>'">
+                        <?= \Helpers\Html::image_tag('css/skins/' . $skin . '/' . $skin . '.png'); ?>
                     </div>
                 </li>
             <?php
@@ -25,5 +26,4 @@ echo ajax_modal_template_header(TEXT_CHANGE_SKIN) ?>
         </ul>
     </div>
 
-<?php
-echo ajax_modal_template_footer('hide-save-button') ?>
+<?= \Helpers\App::ajax_modal_template_footer('hide-save-button') ?>
