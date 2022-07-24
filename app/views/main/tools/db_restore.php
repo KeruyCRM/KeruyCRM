324ff2b8a98f9c2c6239bc2bd60b0563e323c650
KeruyCRM <keruy@keruy.com.ua>
2022-07-24 20:34:30 +0300

    db_restore.php

--- app/views/main/tools/db_restore.php
+++ app/views/main/tools/db_restore.php
@@ -2,19 +2,18 @@
 
 if (!defined('KERUY_CRM')) {
     exit;
-} ?><?php
-echo ajax_modal_template_header(TEXT_WARNING) ?>
+} ?>
+<?= \Helpers\App::ajax_modal_template_header(\K::$fw->TEXT_WARNING) ?>
 
-<?php
-echo form_tag('backup', url_for('tools/db_restore_process', 'action=restore_by_id&id=' . $_GET['id'])); ?>
+<?= \Helpers\Html::form_tag(
+    'backup',
+    \Helpers\Urls::url_for('main/tools/db_restore_process/restore_by_id', 'id=' . \K::$fw->GET['id'])
+); ?>
 
 <div class="modal-body">
-    <?php
-    $backup_info = db_find('app_backups', $_GET['id']);
-    echo sprintf(TEXT_DB_RESTORE_CONFIRMATION, $backup_info['filename']) ?>
+    <?= sprintf(\K::$fw->TEXT_DB_RESTORE_CONFIRMATION, \K::$fw->backup_info['filename']) ?>
 </div>
 
-<?php
-echo ajax_modal_template_footer(TEXT_BUTTON_RESTORE) ?>
+<?= \Helpers\App::ajax_modal_template_footer(\K::$fw->TEXT_BUTTON_RESTORE) ?>
 
-</form>  
+</form>
