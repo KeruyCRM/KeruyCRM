8dea8dbf3752bdfe7b5c0fc83a9f8e1f87b1feef
KeruyCRM <keruy@keruy.com.ua>
2022-07-28 21:32:57 +0300

    menu.php

---
+++ app/controllers/main/entities/menu.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * KeruyCRM (c)
+ * https://keruy.com.ua
+ */
+
+namespace Controllers\Main\Entities;
+
+class Menu extends \Controller
+{
+    private $app_layout = 'layout.php';
+
+    public function __construct()
+    {
+        parent::__construct();
+        \K::security()->checkCsrfToken();
+
+        \Controllers\Main\Entities\_Module::top();
+    }
+
+    public function index()
+    {
+        \K::$fw->subTemplate = \K::$fw->pathSubTemplate . 'menu.php';
+
+        echo \K::view()->render($this->app_layout);
+    }
+}
