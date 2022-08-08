f134499a7b937021a051c5c53b7d84a3ffaf66f3
KeruyCRM <keruy@keruy.com.ua>
2022-08-08 23:23:18 +0300

    fieldtype_action.php

--- app/tools/fieldstypes/fieldtype_action.php
+++ app/tools/fieldstypes/fieldtype_action.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * KeruyCRM (c)
+ * https://keruy.com.ua
+ */
 
 namespace Tools\FieldsTypes;
 
@@ -28,10 +32,10 @@ class Fieldtype_action
             }
         }
 
-        if (users::has_access('delete', $access_rules->get_access_schema())) {
+        if (\Models\Main\Users\Users::has_access('delete', $access_rules->get_access_schema())) {
             $check = true;
 
-            if (users::has_access(
+            if (\Models\Main\Users\Users::has_access(
                     'delete_creator',
                     $access_rules->get_access_schema()
                 ) and $options['item']['created_by'] != \K::$fw->app_user['id']) {
@@ -39,31 +43,34 @@ class Fieldtype_action
             }
 
             if ($check) {
-                $list[] = button_icon_delete(
-                    url_for(
-                        'items/delete',
+                $list[] = \Helpers\Html::button_icon_delete(
+                    \Helpers\Urls::url_for(
+                        'main/items/delete',
                         'id=' . $options['value'] . '&entity_id=' . $options['field']['entities_id'] . '&path=' . $options['path'] . $redirect_to
                     )
                 );
             }
         }
 
-        if (users::has_access('update', $access_rules->get_access_schema())) {
-            $list[] = button_icon_edit(
-                url_for(
-                    'items/form',
+        if (\Models\Main\Users\Users::has_access('update', $access_rules->get_access_schema())) {
+            $list[] = \Helpers\Html::button_icon_edit(
+                \Helpers\Urls::url_for(
+                    'main/items/form',
                     'id=' . $options['value'] . '&entity_id=' . $options['field']['entities_id'] . '&path=' . $options['path'] . $redirect_to
                 )
             );
         }
 
-        //change user pasword
-        if (users::has_access('update', $access_rules->get_access_schema()) and $options['field']['entities_id'] == 1) {
-            $list[] = button_icon(
+        //change user password
+        if (\Models\Main\Users\Users::has_access(
+                'update',
+                $access_rules->get_access_schema()
+            ) and $options['field']['entities_id'] == 1) {
+            $list[] = \Helpers\Html::button_icon(
                 \K::$fw->TEXT_CHANGE_PASSWORD,
                 'fa fa-unlock-alt',
-                url_for(
-                    'items/change_user_password',
+                \Helpers\Urls::url_for(
+                    'main/items/change_user_password',
                     'path=' . $options['path'] . ($options['item']['parent_item_id'] == 0 ? '-' . $options['value'] : '') . $redirect_to
                 ),
                 false
@@ -73,10 +80,10 @@ class Fieldtype_action
         //login as user
         if (\K::$fw->app_user['group_id'] == 0 and $options['field']['entities_id'] == 1) {
             if ($options['item']['field_5'] == 1) {
-                $list[] = button_icon(
+                $list[] = \Helpers\Html::button_icon(
                     \K::$fw->TEXT_BUTTON_LOGIN,
                     'fa fa-sign-in',
-                    url_for('users/login_as', 'users_id=' . $options['item']['id']),
+                    \Helpers\Urls::url_for('main/users/login_as', 'users_id=' . $options['item']['id']),
                     true
                 );
             }
@@ -85,32 +92,36 @@ class Fieldtype_action
         //check access to action with assigned only
         if ($options['hide_actions_buttons'] == 1) {
             $list = [];
-        } else {
-            if (users::has_users_access_name_to_entity('action_with_assigned', $options['field']['entities_id'])) {
-                if (!users::has_access_to_assigned_item($options['field']['entities_id'], $options['item']['id'])) {
-                    $list = [];
-                }
+        } elseif (\Models\Main\Users\Users::has_users_access_name_to_entity(
+            'action_with_assigned',
+            $options['field']['entities_id']
+        )) {
+            if (!\Models\Main\Users\Users::has_access_to_assigned_item(
+                $options['field']['entities_id'],
+                $options['item']['id']
+            )) {
+                $list = [];
             }
         }
 
-        if (isset($options['listing_type']) and $options['listing_type'] == 'tree_table' and users::has_access(
+        if (isset($options['listing_type']) and $options['listing_type'] == 'tree_table' and \Models\Main\Users\Users::has_access(
                 'create',
                 $access_rules->get_access_schema()
             )) {
-            $list[] = button_icon(
+            $list[] = \Helpers\Html::button_icon(
                 \K::$fw->TEXT_BUTTON_CREATE,
                 'fa fa-plus',
-                url_for(
-                    'items/form',
+                \Helpers\Urls::url_for(
+                    'main/items/form',
                     'parent_id=' . $options['item']['id'] . '&entity_id=' . $options['field']['entities_id'] . '&path=' . $options['path'] . $redirect_to
                 )
             );
         } else {
-            $list[] = button_icon(
+            $list[] = \Helpers\Html::button_icon(
                 \K::$fw->TEXT_BUTTON_INFO,
                 'fa fa-info',
-                url_for(
-                    'items/info',
+                \Helpers\Urls::url_for(
+                    'main/items/info',
                     'path=' . (count(
                         $options['path_info']
                     ) ? $options['path'] : $options['path'] . '-' . $options['value']) . '&gotopage[' . \K::$fw->POST['reports_id'] . ']=' . \K::$fw->POST['page']
@@ -119,8 +130,8 @@ class Fieldtype_action
             );
         }
 
-        //hide checkbox for mulitple items update if users don't have access
-        if (count($list) == 1 and !users::has_users_access_name_to_entity(
+        //hide checkbox for multiple items update if users don't have access
+        if (count($list) == 1 and !\Models\Main\Users\Users::has_users_access_name_to_entity(
                 'export_selected',
                 $options['field']['entities_id']
             )) {
