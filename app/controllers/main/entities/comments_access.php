1d4b89a348f62d1e2aac816c4e624d6134897ee2
KeruyCRM <keruy@keruy.com.ua>
2022-08-15 23:15:55 +0300

    comments_access.php

--- app/controllers/main/entities/comments_access.php
+++ app/controllers/main/entities/comments_access.php
@@ -25,34 +25,50 @@ class Comments_access extends \Controller
 
     public function set_access()
     {
-        if (isset($_POST['access'])) {
-            foreach ($_POST['access'] as $access_groups_id => $access) {
-                $sql_data = ['access_schema' => str_replace('_', ',', $access)];
+        if (\K::$fw->VERB == 'POST' and isset(\K::$fw->GET['entities_id'])) {
+            if (isset(\K::$fw->POST['access'])) {
+                \K::model()->begin();
 
-                $acess_info_query = db_query(
-                    "select access_schema from app_comments_access where entities_id='" . db_input(
-                        $_GET['entities_id']
-                    ) . "' and access_groups_id='" . $access_groups_id . "'"
-                );
-                if ($acess_info = db_fetch_array($acess_info_query)) {
-                    db_perform(
-                        'app_comments_access',
-                        $sql_data,
-                        'update',
-                        "entities_id='" . db_input(
+                foreach (\K::$fw->POST['access'] as $access_groups_id => $access) {
+                    $sql_data = ['access_schema' => str_replace('_', ',', $access)];
+                    $sql_data['entities_id'] = \K::$fw->GET['entities_id'];
+                    $sql_data['access_groups_id'] = $access_groups_id;
+
+                    \K::model()->db_perform('app_comments_access', $sql_data, [
+                        'entities_id = ? and access_groups_id = ?',
+                        \K::$fw->GET['entities_id'],
+                        $access_groups_id
+
+                    ]);
+                    /*$acess_info_query = db_query(
+                        "select access_schema from app_comments_access where entities_id='" . db_input(
                             $_GET['entities_id']
                         ) . "' and access_groups_id='" . $access_groups_id . "'"
                     );
-                } else {
-                    $sql_data['entities_id'] = $_GET['entities_id'];
-                    $sql_data['access_groups_id'] = $access_groups_id;
-                    db_perform('app_comments_access', $sql_data);
+                    if ($acess_info = db_fetch_array($acess_info_query)) {
+                        db_perform(
+                            'app_comments_access',
+                            $sql_data,
+                            'update',
+                            "entities_id='" . db_input(
+                                $_GET['entities_id']
+                            ) . "' and access_groups_id='" . $access_groups_id . "'"
+                        );
+                    } else {
+                        $sql_data['entities_id'] = $_GET['entities_id'];
+                        $sql_data['access_groups_id'] = $access_groups_id;
+                        db_perform('app_comments_access', $sql_data);
+                    }*/
                 }
+
+                \K::model()->commit();
+
+                \K::flash()->addMessage(\K::$fw->TEXT_ACCESS_UPDATED, 'success');
             }
 
-            $alerts->add(TEXT_ACCESS_UPDATED, 'success');
+            \Helpers\Urls::redirect_to('main/entities/comments_access', 'entities_id=' . \K::$fw->GET['entities_id']);
+        } else {
+            \Helpers\Urls::redirect_to('main/dashboard');
         }
-
-        redirect_to('entities/comments_access', 'entities_id=' . $_GET['entities_id']);
     }
 }
