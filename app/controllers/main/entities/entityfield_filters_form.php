e4bb24b1ab4bc5c0fd62087d71c4718c1974c2e7
KeruyCRM <keruy@keruy.com.ua>
2022-08-17 13:54:41 +0300

    entityfield_filters_form.php

---
+++ app/controllers/main/entities/entityfield_filters_form.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * KeruyCRM (c)
+ * https://keruy.com.ua
+ */
+
+namespace Controllers\Main\Entities;
+
+class Entityfield_filters_form extends \Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        \K::security()->checkCsrfToken();
+
+        \Controllers\Main\Entities\_Module::top();
+    }
+
+    public function index()
+    {
+        /*$reports_info_query = db_query(
+            "select * from app_reports where id='" . db_input(
+                $_GET['reports_id']
+            ) . "' and reports_type='entityfield" . $_GET['fields_id'] . "'"
+        );*/
+
+        \K::$fw->reports_info = \K::model()->db_fetch_one('app_reports', [
+            'id = ? and reports_type = ?',
+            \K::$fw->GET['reports_id'],
+            'entityfield' . (int)\K::$fw->GET['fields_id']
+        ]);
+
+        if (!\K::$fw->reports_info) {
+            echo \K::$fw->TEXT_REPORT_NOT_FOUND;
+            return;
+        }
+
+        \K::$fw->obj = \K::$fw->db_find('app_reports_filters', \K::$fw->GET['id']);
+
+        \K::$fw->subTemplate = \K::$fw->pathSubTemplate . 'entityfield_filters_form.php';
+
+        echo \K::view()->render(\K::$fw->subTemplate);
+    }
+}
