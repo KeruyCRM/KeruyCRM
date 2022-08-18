e76748f6da3217cd5f4caa05a0265d9e86f6c372
KeruyCRM <keruy@keruy.com.ua>
2022-08-18 17:20:13 +0300

    fields_choices_sort_reset.php

--- app/views/main/entities/fields_choices_sort_reset.php
+++ app/views/main/entities/fields_choices_sort_reset.php
@@ -3,26 +3,22 @@
 if (!defined('KERUY_CRM')) {
     exit;
 } ?>
-<?php
-echo ajax_modal_template_header(TEXT_RESET_SORTING) ?>
+<?= \Helpers\App::ajax_modal_template_header(\K::$fw->TEXT_RESET_SORTING) ?>
 
-<?php
-echo form_tag(
+<?= \Helpers\Html::form_tag(
     'choices_form',
-    url_for(
-        'entities/fields_choices',
-        'action=sort_reset&entities_id=' . $_GET['entities_id'] . '&fields_id=' . $_GET['fields_id']
+    \Helpers\Urls::url_for(
+        'main/entities/fields_choices/sort_reset',
+        'entities_id=' . \K::$fw->GET['entities_id'] . '&fields_id=' . \K::$fw->GET['fields_id']
     ),
     ['class' => 'form-horizontal']
 ) ?>
 <div class="modal-body">
     <div class="form-body">
-        <?php
-        echo TEXT_VALUES_WILL_SORTED_BY_NAME ?>
+        <?= \K::$fw->TEXT_VALUES_WILL_SORTED_BY_NAME ?>
     </div>
 </div>
 
-<?php
-echo ajax_modal_template_footer(TEXT_BUTTON_CONTINUE) ?>
+<?= \Helpers\App::ajax_modal_template_footer(\K::$fw->TEXT_BUTTON_CONTINUE) ?>
 
-</form> 
+</form>
