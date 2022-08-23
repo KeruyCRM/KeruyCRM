da8e2653a224011f9b8eb6ea77455b9df4daaab9
KeruyCRM <keruy@keruy.com.ua>
2022-08-23 12:20:18 +0300

    hide_subentity_filters.php

--- app/views/main/entities/hide_subentity_filters.php
+++ app/views/main/entities/hide_subentity_filters.php
@@ -3,69 +3,51 @@
 if (!defined('KERUY_CRM')) {
     exit;
 } ?>
-<?php
-require(component_path('entities/navigation')) ?>
+<?= \K::view()->render(\Helpers\Urls::components_path('main/entities/navigation')); ?>
 
-    <h3 class="page-title"><?php
-        echo TEXT_HIDE_BY_CONDITION . ' (' . TEXT_ITEM_PAGE_PARENT_ITEM . ')' ?></h3>
+    <h3 class="page-title"><?= \K::$fw->TEXT_HIDE_BY_CONDITION . ' (' . \K::$fw->TEXT_ITEM_PAGE_PARENT_ITEM . ')' ?></h3>
+    <p><?= \K::$fw->TEXT_HIDE_BY_CONDITION_SUBENTITY_INFO ?></p>
 
-    <p><?php
-        echo TEXT_HIDE_BY_CONDITION_SUBENTITY_INFO ?></p>
-
-<?php
-echo button_tag(
-    TEXT_BUTTON_ADD_NEW_REPORT_FILTER,
-    url_for(
-        'entities/hide_subentity_filters_form',
-        'reports_id=' . $reports_info['id'] . '&entities_id=' . $_GET['entities_id']
+<?= \Helpers\Html::button_tag(
+    \K::$fw->TEXT_BUTTON_ADD_NEW_REPORT_FILTER,
+    \Helpers\Urls::url_for(
+        'main/entities/hide_subentity_filters_form',
+        'reports_id=' . \K::$fw->reports_info['id'] . '&entities_id=' . \K::$fw->GET['entities_id']
     )
 );
 ?>
-
     <div class="table-scrollable">
         <table class="table table-striped table-bordered table-hover">
             <thead>
             <tr>
-                <th><?php
-                    echo TEXT_ACTION ?></th>
-                <th width="100%"><?php
-                    echo TEXT_FIELD ?></th>
-                <th><?php
-                    echo TEXT_VALUES ?></th>
-
+                <th><?= \K::$fw->TEXT_ACTION ?></th>
+                <th width="100%"><?= \K::$fw->TEXT_FIELD ?></th>
+                <th><?= \K::$fw->TEXT_VALUES ?></th>
             </tr>
             </thead>
             <tbody>
             <?php
-            $filters_query = db_query(
-                "select rf.*, f.name, f.type from app_reports_filters rf left join app_fields f on rf.fields_id=f.id where rf.reports_id='" . db_input(
-                    $reports_info['id']
-                ) . "' order by rf.id"
-            );
-
-            if (db_num_rows($filters_query) == 0) {
-                echo '<tr><td colspan="5">' . TEXT_NO_RECORDS_FOUND . '</td></tr>';
+            if (count(\K::$fw->filters_query) == 0) {
+                echo '<tr><td colspan="3">' . \K::$fw->TEXT_NO_RECORDS_FOUND . '</td></tr>';
             }
 
-            while ($v = db_fetch_array($filters_query)):
+            //while ($v = db_fetch_array($filters_query)):
+            foreach (\K::$fw->filters_query as $v):
                 ?>
                 <tr>
-                    <td style="white-space: nowrap;"><?php
-                        echo button_icon_delete(
-                                url_for(
-                                    'entities/hide_subentity_filters_delete',
-                                    'id=' . $v['id'] . '&reports_id=' . $reports_info['id'] . '&entities_id=' . $_GET['entities_id']
-                                )
-                            ) . ' ' . button_icon_edit(
-                                url_for(
-                                    'entities/hide_subentity_filters_form',
-                                    'id=' . $v['id'] . '&reports_id=' . $reports_info['id'] . '&entities_id=' . $_GET['entities_id']
-                                )
-                            ) ?></td>
-                    <td><?php
-                        echo fields_types::get_option($v['type'], 'name', $v['name']) ?></td>
-                    <td class="nowrap"><?php
-                        echo reports::render_filters_values(
+                    <td style="white-space: nowrap;"><?= \Helpers\Html::button_icon_delete(
+                            \Helpers\Urls::url_for(
+                                'main/entities/hide_subentity_filters_delete',
+                                'id=' . $v['id'] . '&reports_id=' . \K::$fw->reports_info['id'] . '&entities_id=' . \K::$fw->GET['entities_id']
+                            )
+                        ) . ' ' . \Helpers\Html::button_icon_edit(
+                            \Helpers\Urls::url_for(
+                                'main/entities/hide_subentity_filters_form',
+                                'id=' . $v['id'] . '&reports_id=' . \K::$fw->reports_info['id'] . '&entities_id=' . \K::$fw->GET['entities_id']
+                            )
+                        ) ?></td>
+                    <td><?= \Models\Main\Fields_types::get_option($v['type'], 'name', $v['name']) ?></td>
+                    <td class="nowrap"><?= \Models\Main\Reports\Reports::render_filters_values(
                             $v['fields_id'],
                             $v['filters_values'],
                             '<br>',
@@ -73,13 +55,12 @@ echo button_tag(
                         ) ?></td>
                 </tr>
             <?php
-            endwhile ?>
+            endforeach; ?>
             </tbody>
         </table>
     </div>
 
-<?php
-echo '<a class="btn btn-default" href="' . url_for(
-        'entities/item_page_configuration',
-        'entities_id=' . $entities_info['parent_id']
-    ) . '">' . TEXT_BUTTON_BACK . '</a>'; ?>
+<?= '<a class="btn btn-default" href="' . \Helpers\Urls::url_for(
+    'main/entities/item_page_configuration',
+    'entities_id=' . \K::$fw->entities_info['parent_id']
+) . '">' . \K::$fw->TEXT_BUTTON_BACK . '</a>'; ?>
