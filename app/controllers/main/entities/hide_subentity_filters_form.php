4007459c41c504af38cdc62b30f2fece516c1abc
KeruyCRM <keruy@keruy.com.ua>
2022-08-23 12:54:13 +0300

    hide_subentity_filters_form.php

--- app/controllers/main/entities/hide_subentity_filters_form.php
+++ app/controllers/main/entities/hide_subentity_filters_form.php
@@ -18,25 +18,34 @@ class Hide_subentity_filters_form extends \Controller
 
     public function index()
     {
-        $reports_info_query = db_query(
+        /*$reports_info_query = db_query(
             "select * from app_reports where id='" . db_input(
-                $_GET['reports_id']
-            ) . "' and reports_type='hide_subentity_" . _GET('entities_id') . "'"
-        );
-        if (!$reports_info = db_fetch_array($reports_info_query)) {
-            return TEXT_REPORT_NOT_FOUND;
+                \K::$fw->GET['reports_id']
+            ) . "' and reports_type='hide_subentity_" . \K::$fw->GET['entities_id'] . "'"
+        );*/
+
+        \K::$fw->reports_info = \K::model()->db_fetch_one('app_reports', [
+            'id = ? and reports_type = ?',
+            \K::$fw->GET['reports_id'],
+            'hide_subentity_' . (int)\K::$fw->GET['entities_id']
+        ]);
+
+        if (!\K::$fw->reports_info) {
+            return \K::$fw->TEXT_REPORT_NOT_FOUND;
         }
 
-        $obj = [];
+        /*$obj = [];
 
-        if (isset($_GET['id'])) {
-            $obj = db_find('app_reports_filters', $_GET['id']);
+        if (isset(\K::$fw->GET['id'])) {
+            $obj = db_find('app_reports_filters', \K::$fw->GET['id']);
         } else {
             $obj = db_show_columns('app_reports_filters');
-        }
+        }*/
+
+        \K::$fw->obj = \K::model()->db_find('app_reports_filters', \K::$fw->GET['id']);
 
         \K::$fw->subTemplate = \K::$fw->pathSubTemplate . 'hide_subentity_filters_form.php';
 
-        echo \K::view()->render(\K::$fw->app_layout);
+        echo \K::view()->render(\K::$fw->subTemplate);
     }
 }
