ab3e3c87bb8526957a0e3793fe8987fac810b35b
KeruyCRM <keruy@keruy.com.ua>
2022-08-25 15:31:45 +0300

    listing_filters.php

--- app/controllers/main/entities/listing_filters.php
+++ app/controllers/main/entities/listing_filters.php
@@ -15,28 +15,52 @@ class Listing_filters extends \Controller
 
         \Controllers\Main\Entities\_Module::top();
 
-        $reports_info_query = db_query(
+        /*$reports_info_query = db_query(
             "select * from app_reports where entities_id='" . db_input(
-                $_GET['entities_id']
+                \K::$fw->GET['entities_id']
             ) . "' and reports_type='default'"
-        );
-        if (!$reports_info = db_fetch_array($reports_info_query)) {
+        );*/
+
+        if (!\K::$fw->GET['entities_id']) {
+            \Helpers\Urls::redirect_to('main/entities');//FIX
+        }
+
+        \K::$fw->reports_info = \K::model()->db_fetch_one('app_reports', [
+            'entities_id = ? and reports_type = ?',
+            \K::$fw->GET['entities_id'],
+            'default'
+        ]);
+
+        if (!\K::$fw->reports_info) {
             $sql_data = [
                 'name' => '',
-                'entities_id' => $_GET['entities_id'],
+                'entities_id' => \K::$fw->GET['entities_id'],
                 'reports_type' => 'default',
                 'in_menu' => 0,
                 'in_dashboard' => 0,
                 'created_by' => 0,
             ];
-            db_perform('app_reports', $sql_data);
 
-            redirect_to('entities/listing_filters', 'entities_id=' . $_GET['entities_id']);
+            \K::model()->db_perform('app_reports', $sql_data);
+
+            \Helpers\Urls::redirect_to('main/entities/listing_filters', 'entities_id=' . \K::$fw->GET['entities_id']);
         }
     }
 
     public function index()
     {
+        \K::$fw->reports_filters_count = \K::model()->db_count(
+            'app_reports_filters',
+            \K::$fw->reports_info['id'],
+            'reports_id'
+        );
+
+        \K::$fw->filters_query = \K::model()->db_query_exec(
+            "select rf.*, f.name, f.type from app_reports_filters rf left join app_fields f on rf.fields_id = f.id where rf.reports_id = ? order by rf.id",
+            \K::$fw->reports_info['id'],
+            'app_reports_filters,app_fields'
+        );
+
         \K::$fw->subTemplate = \K::$fw->pathSubTemplate . 'listing_filters.php';
 
         echo \K::view()->render(\K::$fw->app_layout);
@@ -44,45 +68,61 @@ class Listing_filters extends \Controller
 
     public function save()
     {
-        $values = '';
+        if (\K::$fw->VERB == 'POST') {
+            $values = '';
 
-        if (isset($_POST['values'])) {
-            if (is_array($_POST['values'])) {
-                $values = implode(',', $_POST['values']);
-            } else {
-                $values = $_POST['values'];
+            if (isset(\K::$fw->POST['values'])) {
+                if (is_array(\K::$fw->POST['values'])) {
+                    $values = implode(',', \K::$fw->POST['values']);
+                } else {
+                    $values = \K::$fw->POST['values'];
+                }
             }
-        }
-        $sql_data = [
-            'reports_id' => $_GET['reports_id'],
-            'fields_id' => $_POST['fields_id'],
-            'filters_condition' => ($_POST['filters_condition'] ?? ''),
-            'filters_values' => $values,
-        ];
 
-        if (isset($_GET['id'])) {
-            db_perform('app_reports_filters', $sql_data, 'update', "id='" . db_input($_GET['id']) . "'");
+            $sql_data = [
+                'reports_id' => \K::$fw->GET['reports_id'],
+                'fields_id' => \K::$fw->POST['fields_id'],
+                'filters_condition' => (\K::$fw->POST['filters_condition'] ?? ''),
+                'filters_values' => $values,
+            ];
+
+            /*if (isset(\K::$fw->GET['id'])) {
+                db_perform('app_reports_filters', $sql_data, 'update', "id='" . db_input(\K::$fw->GET['id']) . "'");
+            } else {
+                db_perform('app_reports_filters', $sql_data);
+            }*/
+
+            \K::model()->db_perform('app_reports_filters', $sql_data, [
+                'id = ?',
+                \K::$fw->GET['id']
+            ]);
+
+            \Helpers\Urls::redirect_to(
+                'main/entities/listing_filters',
+                'reports_id=' . \K::$fw->GET['reports_id'] . '&entities_id=' . \K::$fw->GET['entities_id']
+            );
         } else {
-            db_perform('app_reports_filters', $sql_data);
+            \Helpers\Urls::redirect_to('main/dashboard');
         }
-
-        redirect_to(
-            'entities/listing_filters',
-            'reports_id=' . $_GET['reports_id'] . '&entities_id=' . $_GET['entities_id']
-        );
     }
 
     public function delete()
     {
-        if (isset($_GET['id'])) {
-            db_query("delete from app_reports_filters where id='" . db_input($_GET['id']) . "'");
+        if (\K::$fw->VERB == 'POST') {
+            if (isset(\K::$fw->GET['id'])) {
+                //db_query("delete from app_reports_filters where id='" . db_input(\K::$fw->GET['id']) . "'");
 
-            $alerts->add(TEXT_WARN_DELETE_FILTER_SUCCESS, 'success');
+                \K::model()->db_delete_row('app_reports_filters', \K::$fw->GET['id']);
 
-            redirect_to(
-                'entities/listing_filters',
-                'reports_id=' . $_GET['reports_id'] . '&entities_id=' . $_GET['entities_id']
-            );
+                \K::flash()->addMessage(\K::$fw->TEXT_WARN_DELETE_FILTER_SUCCESS, 'success');
+
+                \Helpers\Urls::redirect_to(
+                    'main/entities/listing_filters',
+                    'reports_id=' . \K::$fw->GET['reports_id'] . '&entities_id=' . \K::$fw->GET['entities_id']
+                );
+            }
+        } else {
+            \Helpers\Urls::redirect_to('main/dashboard');
         }
     }
 }
--- app/views/main/entities/listing_filters.php
+++ app/views/main/entities/listing_filters.php
@@ -3,93 +3,74 @@
 if (!defined('KERUY_CRM')) {
     exit;
 } ?>
-<?php
-require(component_path('entities/navigation')) ?>
+<?= \K::view()->render(\Helpers\Urls::components_path('main/entities/navigation')); ?>
 
-<h3 class="page-title"><?php
-    echo TEXT_NAV_LISTING_FILTERS_CONFIG ?></h3>
+    <h3 class="page-title"><?= \K::$fw->TEXT_NAV_LISTING_FILTERS_CONFIG ?></h3>
 
-<p><?php
-    echo TEXT_LISTING_FILTERS_CFG_INFO ?></p>
+    <p><?= \K::$fw->TEXT_LISTING_FILTERS_CFG_INFO ?></p>
 
-<?php
-echo button_tag(
-        TEXT_BUTTON_ADD_NEW_REPORT_FILTER,
-        url_for(
-            'entities/listing_filters_form',
-            'reports_id=' . $reports_info['id'] . '&entities_id=' . $_GET['entities_id']
-        )
-    ) . ' ' . button_tag(
-        TEXT_BUTTON_CONFIGURE_SORTING,
-        url_for('reports/sorting', 'reports_id=' . $reports_info['id'] . '&redirect_to=listng_filters')
-    ) ?>
-
-<div class="table-scrollable">
-    <table class="table table-striped table-bordered table-hover">
-        <thead>
-        <tr>
-            <th><?php
-                echo TEXT_ACTION ?></th>
-            <th width="100%"><?php
-                echo TEXT_FIELD ?></th>
-            <th><?php
-                echo TEXT_FILTERS_CONDITION ?></th>
-            <th><?php
-                echo TEXT_VALUES ?></th>
-
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        if (db_count('app_reports_filters', $reports_info['id'], 'reports_id') == 0) {
-            echo '<tr><td colspan="5">' . TEXT_NO_RECORDS_FOUND . '</td></tr>';
-        } ?>
-        <?php
-        $filters_query = db_query(
-            "select rf.*, f.name, f.type from app_reports_filters rf left join app_fields f on rf.fields_id=f.id where rf.reports_id='" . db_input(
-                $reports_info['id']
-            ) . "' order by rf.id"
-        );
-        while ($v = db_fetch_array($filters_query)):
-            ?>
-            <tr>
-                <td style="white-space: nowrap;"><?php
-                    echo button_icon_delete(
-                            url_for(
-                                'entities/listing_filters_delete',
-                                'id=' . $v['id'] . '&reports_id=' . $reports_info['id'] . '&entities_id=' . $_GET['entities_id']
-                            )
-                        ) . ' ' . button_icon_edit(
-                            url_for(
-                                'entities/listing_filters_form',
-                                'id=' . $v['id'] . '&reports_id=' . $reports_info['id'] . '&entities_id=' . $_GET['entities_id']
-                            )
-                        ) ?></td>
-                <td><?php
-                    echo fields_types::get_option($v['type'], 'name', $v['name']) ?></td>
-                <td><?php
-                    echo reports::get_condition_name_by_key($v['filters_condition']) ?></td>
-                <td class="nowrap"><?php
-                    echo reports::render_filters_values(
-                        $v['fields_id'],
-                        $v['filters_values'],
-                        '<br>',
-                        $v['filters_condition']
-                    ) ?></td>
-            </tr>
-        <?php
-        endwhile ?>
-        </tbody>
-    </table>
-</div>
-
-<?php
-echo link_to(
-    TEXT_BUTTON_BACK,
-    url_for('filters_panels/panels', 'entities_id=' . _get::int('entities_id')),
-    ['class' => 'btn btn-default']
+<?= \Helpers\Html::button_tag(
+    \K::$fw->TEXT_BUTTON_ADD_NEW_REPORT_FILTER,
+    \Helpers\Urls::url_for(
+        'main/entities/listing_filters_form',
+        'reports_id=' . \K::$fw->reports_info['id'] . '&entities_id=' . \K::$fw->GET['entities_id']
+    )
+) . ' ' . \Helpers\Html::button_tag(
+    \K::$fw->TEXT_BUTTON_CONFIGURE_SORTING,
+    \Helpers\Urls::url_for(
+        'main/reports/sorting',
+        'reports_id=' . \K::$fw->reports_info['id'] . '&redirect_to=listng_filters'
+    )
 ) ?>
 
+    <div class="table-scrollable">
+        <table class="table table-striped table-bordered table-hover">
+            <thead>
+            <tr>
+                <th><?= \K::$fw->TEXT_ACTION ?></th>
+                <th width="100%"><?= \K::$fw->TEXT_FIELD ?></th>
+                <th><?= \K::$fw->TEXT_FILTERS_CONDITION ?></th>
+                <th><?= \K::$fw->TEXT_VALUES ?></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            if (\K::$fw->reports_filters_count == 0) {
+                echo '<tr><td colspan="4">' . \K::$fw->TEXT_NO_RECORDS_FOUND . '</td></tr>';
+            } ?>
+            <?php
+            //while ($v = db_fetch_array($filters_query)):
+            foreach (\K::$fw->filters_query as $v):
+                ?>
+                <tr>
+                    <td style="white-space: nowrap;"><?= \Helpers\Html::button_icon_delete(
+                            \Helpers\Urls::url_for(
+                                'main/entities/listing_filters_delete',
+                                'id=' . $v['id'] . '&reports_id=' . \K::$fw->reports_info['id'] . '&entities_id=' . \K::$fw->GET['entities_id']
+                            )
+                        ) . ' ' . \Helpers\Html::button_icon_edit(
+                            \Helpers\Urls::url_for(
+                                'main/entities/listing_filters_form',
+                                'id=' . $v['id'] . '&reports_id=' . \K::$fw->reports_info['id'] . '&entities_id=' . \K::$fw->GET['entities_id']
+                            )
+                        ) ?></td>
+                    <td><?= \Models\Main\Fields_types::get_option($v['type'], 'name', $v['name']) ?></td>
+                    <td><?= \Models\Main\Reports\Reports::get_condition_name_by_key($v['filters_condition']) ?></td>
+                    <td class="nowrap"><?= \Models\Main\Reports\Reports::render_filters_values(
+                            $v['fields_id'],
+                            $v['filters_values'],
+                            '<br>',
+                            $v['filters_condition']
+                        ) ?></td>
+                </tr>
+            <?php
+            endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 
-
-
+<?= \Helpers\Urls::link_to(
+    \K::$fw->TEXT_BUTTON_BACK,
+    \Helpers\Urls::url_for('main/filters_panels/panels', 'entities_id=' . \K::$fw->GET['entities_id']),
+    ['class' => 'btn btn-default']
+) ?>
