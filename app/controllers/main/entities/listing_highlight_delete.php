199f21639f541387f73f5de0551745ba66bd3f17
KeruyCRM <keruy@keruy.com.ua>
2022-08-25 23:18:55 +0300

    listing_highlight_delete.php

---
+++ app/controllers/main/entities/listing_highlight_delete.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * KeruyCRM (c)
+ * https://keruy.com.ua
+ */
+
+namespace Controllers\Main\Entities;
+
+class Listing_highlight_delete extends \Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        \K::security()->checkCsrfToken();
+
+        \Controllers\Main\Entities\_Module::top();
+    }
+
+    public function index()
+    {
+        \K::$fw->subTemplate = \K::$fw->pathSubTemplate . 'listing_highlight_delete.php';
+
+        echo \K::view()->render(\K::$fw->subTemplate);
+    }
+}
