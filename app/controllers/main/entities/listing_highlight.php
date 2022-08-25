290fabd59932d482e293601a9e762fd5c261ec77
KeruyCRM <keruy@keruy.com.ua>
2022-08-25 23:14:55 +0300

    listing_highlight.php

--- app/controllers/main/entities/listing_highlight.php
+++ app/controllers/main/entities/listing_highlight.php
@@ -23,55 +23,88 @@ class Listing_highlight extends \Controller
 
     public function save()
     {
-        $sql_data = [
-            'is_active' => (isset($_POST['is_active']) ? true : false),
-            'fields_id' => _POST('fields_id'),
-            'fields_values' => (is_array($_POST['fields_values']) ? implode(
-                ',',
-                $_POST['fields_values']
-            ) : $_POST['fields_values']),
-            'entities_id' => _get::int('entities_id'),
-            'bg_color' => $_POST['bg_color'],
-            'sort_order' => $_POST['sort_order'],
-            'notes' => $_POST['notes'],
-        ];
+        if (\K::$fw->VERB == 'POST' and isset(\K::$fw->GET['entities_id'])) {
+            $sql_data = [
+                'is_active' => isset(\K::$fw->POST['is_active']),
+                'fields_id' => \K::$fw->POST['fields_id'],
+                'fields_values' => (is_array(\K::$fw->POST['fields_values']) ? implode(
+                    ',',
+                    \K::$fw->POST['fields_values']
+                ) : \K::$fw->POST['fields_values']),
+                'entities_id' => \K::$fw->GET['entities_id'],
+                'bg_color' => \K::$fw->POST['bg_color'],
+                'sort_order' => \K::$fw->POST['sort_order'],
+                'notes' => \K::$fw->POST['notes'],
+            ];
 
-        if (isset($_GET['id'])) {
-            db_perform('app_listing_highlight_rules', $sql_data, 'update', "id='" . db_input($_GET['id']) . "'");
+            /*if (isset(\K::$fw->GET['id'])) {
+                db_perform(
+                    'app_listing_highlight_rules',
+                    $sql_data,
+                    'update',
+                    "id='" . db_input(\K::$fw->GET['id']) . "'"
+                );
+            } else {
+                db_perform('app_listing_highlight_rules', $sql_data);
+            }*/
+
+            \K::model()->db_perform('app_listing_highlight_rules', $sql_data, [
+                'id = ?',
+                \K::$fw->GET['id']
+            ]);
+
+            \Helpers\Urls::redirect_to('main/entities/listing_types', 'entities_id=' . \K::$fw->GET['entities_id']);
         } else {
-            db_perform('app_listing_highlight_rules', $sql_data);
+            \Helpers\Urls::redirect_to('main/dashboard');
         }
-
-        redirect_to('entities/listing_types', 'entities_id=' . _GET('entities_id'));
     }
 
     public function delete()
     {
-        db_delete_row('app_listing_highlight_rules', _GET('id'));
-        redirect_to('entities/listing_types', 'entities_id=' . _GET('entities_id'));
+        if (\K::$fw->VERB == 'POST' and isset(\K::$fw->GET['id']) and isset(\K::$fw->GET['entities_id'])) {
+            \K::model()->db_delete_row('app_listing_highlight_rules', \K::$fw->GET['id']);
+
+            \Helpers\Urls::redirect_to('main/entities/listing_types', 'entities_id=' . \K::$fw->GET['entities_id']);
+        } else {
+            \Helpers\Urls::redirect_to('main/dashboard');
+        }
     }
 
     public function get_field_value()
     {
-        $obj = (isset($_POST['id']) ? db_find('app_listing_highlight_rules', _POST('id')) : db_show_columns(
+        /*$obj = (isset(\K::$fw->POST['id']) ? db_find(
+            'app_listing_highlight_rules',
+            \K::$fw->POST['id']
+        ) : db_show_columns(
             'app_listing_highlight_rules'
-        ));
+        ));*/
+
+        $obj = \K::model()->db_find('app_listing_highlight_rules', \K::$fw->POST['id']);
 
         $html = '';
-        $field_query = db_query("select id,name,type,configuration from app_fields where id=" . _POST('fields_id'));
-        if ($field_info = db_fetch_array($field_query)) {
-            $values_html_heading = TEXT_VALUES;
+        /*$field_query = db_query(
+            "select id,name,type,configuration from app_fields where id=" . \K::$fw->POST['fields_id']
+        );**/
+
+        $field_info = \K::model()->db_fetch_one('app_fields', [
+            'id = ?',
+            \K::$fw->POST['fields_id']
+        ], [], 'id,name,type,configuration');
+
+        if ($field_info) {
+            $values_html_heading = \K::$fw->TEXT_VALUES;
             $values_html = '';
 
-            $cfg = new fields_types_cfg($field_info['configuration']);
+            $cfg = new \Models\Main\Fields_types_cfg($field_info['configuration']);
 
-            switch (listing_highlight::get_field_type_key($field_info['type'])) {
+            switch (\Models\Main\Items\Listing_highlight::get_field_type_key($field_info['type'])) {
                 case 'boolean':
                     $choices = [
-                        'true' => TEXT_BOOLEAN_TRUE,
-                        'false' => TEXT_BOOLEAN_FALSE,
+                        'true' => \K::$fw->TEXT_BOOLEAN_TRUE,
+                        'false' => \K::$fw->TEXT_BOOLEAN_FALSE,
                     ];
-                    $values_html = select_tag(
+
+                    $values_html = \Helpers\Html::select_tag(
                         'fields_values[]',
                         $choices,
                         $obj['fields_values'],
@@ -80,12 +113,12 @@ class Listing_highlight extends \Controller
                     break;
                 case 'choices':
                     if ($cfg->get('use_global_list') > 0) {
-                        $choices = global_lists::get_choices($cfg->get('use_global_list'), false);
+                        $choices = \Models\Main\Global_lists::get_choices($cfg->get('use_global_list'), false);
                     } else {
-                        $choices = fields_choices::get_choices($field_info['id'], false);
+                        $choices = \Models\Main\Fields_choices::get_choices($field_info['id'], false);
                     }
 
-                    $values_html = select_tag(
+                    $values_html = \Helpers\Html::select_tag(
                         'fields_values[]',
                         $choices,
                         $obj['fields_values'],
@@ -94,14 +127,14 @@ class Listing_highlight extends \Controller
                     break;
                 case 'entities':
                     $parent_entity_item_is_the_same = false;
-                    $choices = fieldtype_entity::get_choices(
+                    $choices = \Tools\FieldsTypes\Fieldtype_entity::get_choices(
                         $field_info,
                         ['parent_entity_item_id' => 0],
                         '',
                         $parent_entity_item_is_the_same
                     );
 
-                    $values_html = select_tag(
+                    $values_html = \Helpers\Html::select_tag(
                         'fields_values[]',
                         $choices,
                         $obj['fields_values'],
@@ -110,16 +143,18 @@ class Listing_highlight extends \Controller
 
                     break;
                 case 'dates':
-                    $values_html_heading = TEXT_FILTER_BY_DAYS;
-                    $values_html = input_tag(
+                    $values_html_heading = \K::$fw->TEXT_FILTER_BY_DAYS;
+                    $values_html = \Helpers\Html::input_tag(
                             'fields_values',
                             $obj['fields_values'],
                             ['class' => 'form-control required']
-                        ) . tooltip_text(TEXT_FILTER_BY_DAYS_TOOLTIP);
+                        ) . \Helpers\App::tooltip_text(\K::$fw->TEXT_FILTER_BY_DAYS_TOOLTIP);
                     break;
                 case 'numeric':
-                    $values_html_heading = tooltip_icon(TEXT_FILTERS_NUMERIC_FIELDS_TOOLTIP) . TEXT_VALUES;
-                    $values_html = input_tag(
+                    $values_html_heading = \Helpers\App::tooltip_icon(
+                            \K::$fw->TEXT_FILTERS_NUMERIC_FIELDS_TOOLTIP
+                        ) . \K::$fw->TEXT_VALUES;
+                    $values_html = \Helpers\Html::input_tag(
                         'fields_values',
                         $obj['fields_values'],
                         ['class' => 'form-control required']
