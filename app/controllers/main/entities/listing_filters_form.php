0cd349c9e3042105a473c82abeed9d5de1401ce5
KeruyCRM <keruy@keruy.com.ua>
2022-08-25 22:45:38 +0300

    listing_filters_form.php

--- app/controllers/main/entities/listing_filters_form.php
+++ app/controllers/main/entities/listing_filters_form.php
@@ -18,24 +18,32 @@ class Listing_filters_form extends \Controller
 
     public function index()
     {
-        $reports_info_query = db_query(
-            "select * from app_reports where id='" . db_input($_GET['reports_id']) . "' and reports_type='default'"
-        );
-        if (!$reports_info = db_fetch_array($reports_info_query)) {
-            echo TEXT_REPORT_NOT_FOUND;
-            exit();
-        }
+        /*$reports_info_query = db_query(
+            "select * from app_reports where id='" . db_input(\K::$fw->GET['reports_id']) . "' and reports_type='default'"
+        );*/
 
-        $obj = [];
+        \K::$fw->reports_info = \K::model()->db_fetch_one('app_reports', [
+            'id = ? and reports_type = ?',
+            \K::$fw->GET['reports_id'],
+            'default'
+        ]);
 
-        if (isset($_GET['id'])) {
-            $obj = db_find('app_reports_filters', $_GET['id']);
+        if (!\K::$fw->reports_info) {
+            echo \K::$fw->TEXT_REPORT_NOT_FOUND;
         } else {
-            $obj = db_show_columns('app_reports_filters');
+            /*$obj = [];
+
+            if (isset(\K::$fw->GET['id'])) {
+                $obj = db_find('app_reports_filters', \K::$fw->GET['id']);
+            } else {
+                $obj = db_show_columns('app_reports_filters');
+            }*/
+
+            \K::$fw->obj = \K::model()->db_find('app_reports_filters', \K::$fw->GET['id']);
+
+            \K::$fw->subTemplate = \K::$fw->pathSubTemplate . 'listing_filters_form.php';
+
+            echo \K::view()->render(\K::$fw->subTemplate);
         }
-
-        \K::$fw->subTemplate = \K::$fw->pathSubTemplate . 'listing_filters_form.php';
-
-        echo \K::view()->render(\K::$fw->subTemplate);
     }
 }
