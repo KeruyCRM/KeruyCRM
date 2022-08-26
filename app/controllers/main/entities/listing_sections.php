485209916ed22320a39ab9d65ed1129b2e9a2b5a
KeruyCRM <keruy@keruy.com.ua>
2022-08-26 14:30:29 +0300

    listing_sections.php

---
+++ app/controllers/main/entities/listing_sections.php
@@ -0,0 +1,91 @@
+<?php
+/*
+ * KeruyCRM (c)
+ * https://keruy.com.ua
+ */
+
+namespace Controllers\Main\Entities;
+
+class Listing_sections extends \Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        \K::security()->checkCsrfToken();
+
+        \Controllers\Main\Entities\_Module::top();
+
+        $listing_types_id = _get::int('listing_types_id');
+
+        $listing_types_query = db_query(
+            "select * from app_listing_types where id='" . _get::int('listing_types_id') . "'"
+        );
+        if (!$listing_types = db_fetch_array($listing_types_query)) {
+            redirect_to('entities/listing_types', 'entities_id=' . _get::int('entities_id'));
+        }
+    }
+
+    public function index()
+    {
+        \K::$fw->subTemplate = \K::$fw->pathSubTemplate . 'listing_sections.php';
+
+        echo \K::view()->render(\K::$fw->app_layout);
+    }
+
+    public function save()
+    {
+        $sql_data = [
+            'listing_types_id' => $listing_types['id'],
+            'name' => $_POST['name'],
+            'fields' => (isset($_POST['fields']) ? implode(',', $_POST['fields']) : ''),
+            'display_field_names' => (isset($_POST['display_field_names']) ? 1 : 0),
+            'sort_order' => $_POST['sort_order'],
+            'text_align' => $_POST['text_align'],
+            'display_as' => $_POST['display_as'],
+            'width' => (isset($_POST['width']) ? $_POST['width'] : ''),
+        ];
+
+        if (isset($_GET['id'])) {
+            db_perform('app_listing_sections', $sql_data, 'update', "id='" . db_input($_GET['id']) . "'");
+        } else {
+            db_perform('app_listing_sections', $sql_data);
+        }
+
+        redirect_to(
+            'entities/listing_sections',
+            'listing_types_id=' . $listing_types['id'] . '&entities_id=' . $_GET['entities_id']
+        );
+    }
+
+    public function delete()
+    {
+        if (isset($_GET['id'])) {
+            db_delete_row('app_listing_sections', _get::int('id'));
+
+            redirect_to(
+                'entities/listing_sections',
+                'listing_types_id=' . $listing_types['id'] . '&entities_id=' . $_GET['entities_id']
+            );
+        }
+    }
+
+    public function sort()
+    {
+        $choices_sorted = $_POST['choices_sorted'];
+
+        if (strlen($choices_sorted) > 0) {
+            $choices_sorted = json_decode(stripslashes($choices_sorted), true);
+
+            $sort_order = 1;
+            foreach ($choices_sorted as $v) {
+                db_query("update app_listing_sections set sort_order={$sort_order} where id={$v['id']}");
+                $sort_order++;
+            }
+        }
+
+        redirect_to(
+            'entities/listing_sections',
+            'listing_types_id=' . $listing_types['id'] . '&entities_id=' . $_GET['entities_id']
+        );
+    }
+}
