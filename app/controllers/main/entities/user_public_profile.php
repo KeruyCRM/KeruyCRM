21c88e2f656ed9102c38248826d466a6926c810d
KeruyCRM <keruy@keruy.com.ua>
2022-08-29 17:21:13 +0300

    user_public_profile.php

--- app/controllers/main/entities/user_public_profile.php
+++ app/controllers/main/entities/user_public_profile.php
@@ -15,37 +15,64 @@ class User_public_profile extends \Controller
 
         \Controllers\Main\Entities\_Module::top();
 
-        if ($_GET['entities_id'] != 1) {
-            redirect_to('entities/entities_configuration', 'entities_id=1');
+        if (\K::$fw->GET['entities_id'] != 1) {
+            \Helpers\Urls::redirect_to('main/entities/entities_configuration', 'entities_id=1');
         }
 
-        $cfq_query = db_query(
+        /*$cfq_query = db_query(
             "select * from app_configuration where configuration_name='CFG_PUBLIC_USER_PROFILE_FIELDS'"
-        );
-        if (!$cfq = db_fetch_array($cfq_query)) {
-            db_perform(
+        );*/
+
+        $cfq = \K::model()->db_fetch_one('app_configuration', [
+            'configuration_name = ?',
+            'CFG_PUBLIC_USER_PROFILE_FIELDS'
+        ]);
+
+        if (!$cfq) {
+            \K::model()->db_perform(
                 'app_configuration',
                 ['configuration_value' => '', 'configuration_name' => 'CFG_PUBLIC_USER_PROFILE_FIELDS']
             );
-            redirect_to('entities/user_public_profile', 'entities_id=1');
+
+            \Helpers\Urls::redirect_to('main/entities/user_public_profile', 'entities_id=1');
         }
 
-        $fields_query = db_query(
-            "select f.*, t.name as tab_name from app_fields f, app_forms_tabs t where f.type not in (" . fields_types::get_reserverd_types_list(
-            ) . "," . fields_types::get_users_types_list() . ") and f.entities_id='" . db_input(
-                $_GET['entities_id']
-            ) . "' and f.forms_tabs_id=t.id"
+        $fields_query = \K::model()->db_query_exec_one(
+            "select f.*, t.name as tab_name from app_fields f, app_forms_tabs t where f.type not in (" . \Models\Main\Fields_types::get_reserverd_types_list(
+            ) . "," . \Models\Main\Fields_types::get_users_types_list(
+            ) . ") and f.entities_id = ? and f.forms_tabs_id = t.id",
+            \K::$fw->GET['entities_id'],
+            'app_fields,app_forms_tabs'
         );
-        if (!$v = db_fetch_array($fields_query)) {
-            $alerts->add(TEXT_USER_PUBLIC_PROFILE_NO_FIELDS, 'warning');
+
+        if (!$fields_query) {
+            \K::flash()->addMessage(\K::$fw->TEXT_USER_PUBLIC_PROFILE_NO_FIELDS, 'warning');
         }
     }
 
     public function index()
     {
         $public_user_profile_fields = (strlen(
-            CFG_PUBLIC_USER_PROFILE_FIELDS
-        ) == 0 ? '0' : CFG_PUBLIC_USER_PROFILE_FIELDS);
+            \K::$fw->CFG_PUBLIC_USER_PROFILE_FIELDS
+        ) == 0 ? '0' : \K::$fw->CFG_PUBLIC_USER_PROFILE_FIELDS);
+
+        \K::$fw->fields_query_in = \K::model()->db_query_exec(
+            "select f.*, t.name as tab_name from app_fields f, app_forms_tabs t where f.type not in (" . \Models\Main\Fields_types::get_reserverd_types_list(
+            ) . "," . \K::model()->quoteToString(
+                ['fieldtype_section', 'fieldtype_user_photo', 'fieldtype_user_skin', 'fieldtype_user_language']
+            ) . ") and f.id in (" . $public_user_profile_fields . ") and f.entities_id = ? and f.forms_tabs_id = t.id order by field(f.id," . $public_user_profile_fields . ")",
+            \K::$fw->GET['entities_id'],
+            'app_fields,app_forms_tabs'
+        );
+
+        \K::$fw->fields_query_notin = \K::model()->db_query_exec(
+            "select f.*, t.name as tab_name from app_fields f, app_forms_tabs t where f.type not in (" . \Models\Main\Fields_types::get_reserverd_types_list(
+            ) . "," . \K::model()->quoteToString(
+                ['fieldtype_section', 'fieldtype_user_photo', 'fieldtype_user_skin', 'fieldtype_user_language']
+            ) . ") and f.id not in (" . $public_user_profile_fields . ") and f.entities_id = ? and f.forms_tabs_id = t.id order by t.sort_order, t.name, f.sort_order, f.name",
+            \K::$fw->GET['entities_id'],
+            'app_fields,app_forms_tabs'
+        );
 
         \K::$fw->subTemplate = \K::$fw->pathSubTemplate . 'user_public_profile.php';
 
@@ -54,16 +81,19 @@ class User_public_profile extends \Controller
 
     public function sort_fields()
     {
-        $fields_list = [];
-        foreach (explode(',', $_POST['fields_in_profile']) as $v) {
-            $fields_list[] = str_replace('form_fields_', '', $v);
-        }
+        if (\K::$fw->VERB == 'POST') {
+            $fields_list = [];
+            foreach (explode(',', \K::$fw->POST['fields_in_profile']) as $v) {
+                $fields_list[] = str_replace('form_fields_', '', $v);
+            }
 
-        db_perform(
-            'app_configuration',
-            ['configuration_value' => implode(',', $fields_list)],
-            'update',
-            "configuration_name='CFG_PUBLIC_USER_PROFILE_FIELDS'"
-        );
+            \K::model()->db_update('app_configuration', ['configuration_value' => implode(',', $fields_list)], [
+                    'configuration_name = ?',
+                    'CFG_PUBLIC_USER_PROFILE_FIELDS'
+                ]
+            );
+        } else {
+            \Helpers\Urls::redirect_to('main/dashboard');
+        }
     }
 }
--- app/views/main/entities/user_public_profile.php
+++ app/views/main/entities/user_public_profile.php
@@ -3,33 +3,23 @@
 if (!defined('KERUY_CRM')) {
     exit;
 } ?>
-<?php
-require(component_path('entities/navigation')) ?>
+<?= \K::view()->render(\Helpers\Urls::components_path('main/entities/navigation')); ?>
 
+<h3 class="page-title"><?= \K::$fw->TEXT_NAV_USER_PUBLIC_PROFILE_CONFIG ?></h3>
 
-<h3 class="page-title"><?php
-    echo TEXT_NAV_USER_PUBLIC_PROFILE_CONFIG ?></h3>
-
-<div><?php
-    echo TEXT_USER_PUBLIC_PROFILE_CFG_INFO ?></div>
+<div><?= \K::$fw->TEXT_USER_PUBLIC_PROFILE_CFG_INFO ?></div>
 
 <table style="width: 100%; max-width: 960px;">
     <tr>
         <td valign="top" width="50%">
             <fieldset>
-                <legend><?php
-                    echo TEXT_FIELDS_IN_USER_PUBLIC_PROFILE ?></legend>
+                <legend><?= \K::$fw->TEXT_FIELDS_IN_USER_PUBLIC_PROFILE ?></legend>
                 <div class="cfg_listing">
                     <ul id="fields_in_profile" class="sortable">
                         <?php
-                        $fields_query = db_query(
-                            "select f.*, t.name as tab_name from app_fields f, app_forms_tabs t where f.type not in (" . fields_types::get_reserverd_types_list(
-                            ) . ",'fieldtype_section','fieldtype_user_photo','fieldtype_user_skin','fieldtype_user_language') and f.id in (" . $public_user_profile_fields . ") and  f.entities_id='" . db_input(
-                                $_GET['entities_id']
-                            ) . "' and f.forms_tabs_id=t.id order by  field(f.id," . $public_user_profile_fields . ")"
-                        );
-                        while ($v = db_fetch_array($fields_query)) {
-                            echo '<li id="form_fields_' . $v['id'] . '"><div>' . fields_types::get_option(
+                        //while ($v = db_fetch_array($fields_query)) {
+                        foreach (\K::$fw->fields_query_in as $v) {
+                            echo '<li id="form_fields_' . $v['id'] . '"><div>' . \Models\Main\Fields_types::get_option(
                                     $v['type'],
                                     'name',
                                     $v['name']
@@ -38,26 +28,17 @@ require(component_path('entities/navigation')) ?>
                         ?>
                     </ul>
                 </div>
-
             </fieldset>
-
         </td>
         <td style="padding-left: 25px;" valign="top">
-
             <fieldset>
-                <legend><?php
-                    echo TEXT_FIELDS_EXCLUDED_FROM_USER_PUBLIC_PROFILE ?></legend>
+                <legend><?= \K::$fw->TEXT_FIELDS_EXCLUDED_FROM_USER_PUBLIC_PROFILE ?></legend>
                 <div class="cfg_listing">
                     <ul id="fields_excluded_from_profile" class="sortable">
                         <?php
-                        $fields_query = db_query(
-                            "select f.*, t.name as tab_name from app_fields f, app_forms_tabs t where f.type not in (" . fields_types::get_reserverd_types_list(
-                            ) . ",'fieldtype_section','fieldtype_user_photo','fieldtype_user_skin','fieldtype_user_language') and f.id not in (" . $public_user_profile_fields . ") and  f.entities_id='" . db_input(
-                                $_GET['entities_id']
-                            ) . "' and f.forms_tabs_id=t.id order by t.sort_order, t.name, f.sort_order, f.name"
-                        );
-                        while ($v = db_fetch_array($fields_query)) {
-                            echo '<li id="form_fields_' . $v['id'] . '"><div>' . fields_types::get_option(
+                        //while ($v = db_fetch_array($fields_query)) {
+                        foreach (\K::$fw->fields_query_notin as $v) {
+                            echo '<li id="form_fields_' . $v['id'] . '"><div>' . \Models\Main\Fields_types::get_option(
                                     $v['type'],
                                     'name',
                                     $v['name']
@@ -67,12 +48,9 @@ require(component_path('entities/navigation')) ?>
                     </ul>
                 </div>
             </fieldset>
-
-
         </td>
     </tr>
 </table>
-
 
 <script>
     $(function () {
@@ -86,17 +64,13 @@ require(component_path('entities/navigation')) ?>
                 data = data.slice(1)
                 $.ajax({
                     type: "POST",
-                    url: '<?php echo url_for(
-                        "entities/user_public_profile",
-                        "action=sort_fields&entities_id=" . $_GET["entities_id"]
+                    url: '<?= \Helpers\Urls::url_for(
+                        'main/entities/user_public_profile/sort_fields',
+                        'entities_id=' . \K::$fw->GET["entities_id"]
                     )?>',
                     data: data
                 });
             }
         });
-
     });
 </script>
-
-
-
