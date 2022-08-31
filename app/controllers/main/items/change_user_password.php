414a0b2fdf0d83e78812980933764fd4496d1cec
KeruyCRM <keruy@keruy.com.ua>
2022-08-31 21:59:53 +0300

    change_user_password.php

--- app/controllers/main/items/change_user_password.php
+++ app/controllers/main/items/change_user_password.php
@@ -15,12 +15,12 @@ class Change_user_password extends \Controller
 
         \Controllers\Main\Items\_Module::top();
 
-        if ($current_entity_id != 1) {
-            redirect_to('dashboard/access_forbidden');
+        if (\K::$fw->current_entity_id != 1) {
+            \Helpers\Urls::redirect_to('main/dashboard/access_forbidden');
         }
 
-        if (!users::has_access('update')) {
-            redirect_to('dashboard/access_forbidden');
+        if (!\Models\Main\Users\Users::has_access('update')) {
+            \Helpers\Urls::redirect_to('main/dashboard/access_forbidden');
         }
     }
 
@@ -33,59 +33,64 @@ class Change_user_password extends \Controller
 
     public function change()
     {
-        $password = $_POST['password_new'];
-        $password_confirm = $_POST['password_confirmation'];
+        if (\K::$fw->VERB == 'POST') {
+            $password = \K::$fw->POST['password_new'];
+            $password_confirm = \K::$fw->POST['password_confirmation'];
 
-        $error = false;
+            $error = false;
 
-        if ($password != $password_confirm) {
-            $error = true;
-            $alerts->add(TEXT_ERROR_PASSWORD_CONFIRMATION, 'error');
-        }
-
-        if (strlen($password) < CFG_PASSWORD_MIN_LENGTH) {
-            $error = true;
-            $alerts->add(TEXT_ERROR_PASSWORD_LENGTH, 'error');
-        }
-
-        if (CFG_IS_STRONG_PASSWORD) {
-            if (!preg_match('/[A-Z]/', $password) or !preg_match('/[0-9]/', $password) or !preg_match(
-                    '/[^\w]/',
-                    $password
-                )) {
+            if ($password != $password_confirm) {
                 $error = true;
-                $alerts->add(TEXT_STRONG_PASSWORD_TIP, 'error');
+                \K::flash()->addMessage(\K::$fw->TEXT_ERROR_PASSWORD_CONFIRMATION, 'error');
             }
+
+            if (strlen($password) < \K::$fw->CFG_PASSWORD_MIN_LENGTH) {
+                $error = true;
+                \K::flash()->addMessage(\K::$fw->TEXT_ERROR_PASSWORD_LENGTH, 'error');
+            }
+
+            if (\K::$fw->CFG_IS_STRONG_PASSWORD) {
+                if (!preg_match('/[A-Z]/', $password) or !preg_match('/[0-9]/', $password) or !preg_match(
+                        '/\W/',
+                        $password
+                    )) {
+                    $error = true;
+                    \K::flash()->addMessage(\K::$fw->TEXT_STRONG_PASSWORD_TIP, 'error');
+                }
+            }
+
+            if (!$error) {
+                //$hasher = new PasswordHash(11, false);
+
+                $sql_data = [];
+                $sql_data['password'] = \K::security()->password_hash($password);
+
+                \K::model()->db_perform('app_entity_1', $sql_data, [
+                    'id = ?',
+                    \K::$fw->current_item_id
+                ]);
+
+                $obj = \K::model()->db_find('app_entity_1', \K::$fw->current_item_id);
+
+                $options = [
+                    'to' => $obj['field_9'],
+                    'to_name' => $obj['field_7'] . ' ' . $obj['field_8'],
+                    'subject' => \K::$fw->TEXT_USER_PWD_CHANGED_EMAIL_SUBJECT,
+                    'body' => \K::$fw->TEXT_USER_PWD_CHANGED_EMAIL_BODY . '<p><b>' . \K::$fw->TEXT_LOGIN_DETAILS . '</b></p><p>' . \K::$fw->TEXT_USERNAME . ': ' . $obj['field_12'] . '<br>' . \K::$fw->TEXT_PASSWORD . ': ' . $password . '</p><p><a href="' . \Helpers\Urls::url_for(
+                            'main/users/login'
+                        ) . '">' . \Helpers\Urls::url_for('main/users/login') . '</a></p>',
+                    'from' => \K::$fw->app_user['email'],
+                    'from_name' => 'noreply'
+                ];
+
+                \Models\Main\Users\Users::send_email($options);
+
+                \K::flash()->addMessage(\K::$fw->TEXT_USER_PASSWORD_UPDATED, 'success');
+            }
+
+            \Helpers\Urls::redirect_to('main/items/change_user_password', 'path=' . \K::$fw->current_path);
+        } else {
+            \Helpers\Urls::redirect_to('main/dashboard');
         }
-
-        if (!$error) {
-            $hasher = new PasswordHash(11, false);
-
-            $sql_data = [];
-            $sql_data['password'] = $hasher->HashPassword($password);
-
-            db_perform('app_entity_1', $sql_data, 'update', "id='" . db_input($current_item_id) . "'");
-
-            $obj = db_find('app_entity_1', $current_item_id);
-
-            $options = [
-                'to' => $obj['field_9'],
-                'to_name' => $obj['field_7'] . ' ' . $obj['field_8'],
-                'subject' => TEXT_USER_PWD_CHANGED_EMAIL_SUBJECT,
-                'body' => TEXT_USER_PWD_CHANGED_EMAIL_BODY . '<p><b>' . TEXT_LOGIN_DETAILS . '</b></p><p>' . TEXT_USERNAME . ': ' . $obj['field_12'] . '<br>' . TEXT_PASSWORD . ': ' . $password . '</p><p><a href="' . url_for(
-                        'users/login',
-                        '',
-                        true
-                    ) . '">' . url_for('users/login', '', true) . '</a></p>',
-                'from' => $app_user['email'],
-                'from_name' => 'noreply'
-            ];
-
-            users::send_email($options);
-
-            $alerts->add(TEXT_USER_PASSWORD_UPDATED, 'success');
-        }
-
-        redirect_to('items/change_user_password', 'path=' . $current_path);
     }
 }
--- app/views/main/items/change_user_password.php
+++ app/views/main/items/change_user_password.php
@@ -4,55 +4,44 @@ if (!defined('KERUY_CRM')) {
     exit;
 } ?>
 <ul class="page-breadcrumb breadcrumb">
-    <?php
-    echo items::render_breadcrumb($app_breadcrumb) ?>
+    <?= \Models\Main\Items\Items::render_breadcrumb(\K::$fw->app_breadcrumb) ?>
 </ul>
 
-<h3 class="page-title"><?php
-    echo TEXT_HEADING_CHANGE_PASSWORD ?></h3>
+<h3 class="page-title"><?= \K::$fw->TEXT_HEADING_CHANGE_PASSWORD ?></h3>
 
-
-<?php
-echo form_tag(
+<?= \Helpers\Html::form_tag(
     'change_password_form',
-    url_for('items/change_user_password', 'action=change&path=' . $_GET['path']),
+    \Helpers\Urls::url_for('main/items/change_user_password/change', 'path=' . \K::$fw->GET['path']),
     ['class' => 'form-horizontal']
 ) ?>
 
 <div class="form-body">
-
     <div class="form-group">
-        <label class="col-md-3 control-label" for="password_new"><?php
-            echo TEXT_NEW_PASSWORD ?></label>
+        <label class="col-md-3 control-label" for="password_new"><?= \K::$fw->TEXT_NEW_PASSWORD ?></label>
         <div class="col-md-9">
-            <?php
-            echo input_password_tag(
+            <?= \Helpers\Html::input_password_tag(
                 'password_new',
                 ['autocomplete' => 'off', 'class' => 'form-control input-medium required']
             ) ?>
         </div>
     </div>
-
     <div class="form-group">
-        <label class="col-md-3 control-label" for="password_confirmation"><?php
-            echo TEXT_PASSWORD_CONFIRMATION ?></label>
+        <label class="col-md-3 control-label"
+               for="password_confirmation"><?= \K::$fw->TEXT_PASSWORD_CONFIRMATION ?></label>
         <div class="col-md-9">
-            <?php
-            echo input_password_tag(
+            <?= \Helpers\Html::input_password_tag(
                 'password_confirmation',
                 ['autocomplete' => 'off', 'class' => 'form-control input-medium  required']
             ) ?>
         </div>
     </div>
-
-    <?php
-    echo submit_tag(TEXT_BUTTON_CHANGE, ['class' => 'btn btn-primary']) ?>
-
+    <?= \Helpers\Html::submit_tag(\K::$fw->TEXT_BUTTON_CHANGE, ['class' => 'btn btn-primary']) ?>
 </div>
+
 </form>
 
 <script>
     $(function () {
         $('#change_password_form').validate();
     });
-</script> 
+</script>
