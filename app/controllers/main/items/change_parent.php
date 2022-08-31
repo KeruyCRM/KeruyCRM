064a635427fa79fe4ddbdf04e2935eb1f7511f7d
KeruyCRM <keruy@keruy.com.ua>
2022-08-31 21:15:06 +0300

    change_parent.php

--- app/controllers/main/items/change_parent.php
+++ app/controllers/main/items/change_parent.php
@@ -20,17 +20,28 @@ class Change_parent extends \Controller
     {
         \K::$fw->subTemplate = \K::$fw->pathSubTemplate . 'change_parent.php';
 
-        echo \K::view()->render(\K::$fw->app_layout);
+        echo \K::view()->render(\K::$fw->subTemplate);
     }
 
     public function change_parent()
     {
-        $parent_id = isset($_POST['parent_id']) ? _POST('parent_id') : 0;
+        if (\K::$fw->VERB == 'POST') {
+            $parent_id = \K::$fw->POST['parent_id'] ?? 0;
 
-        if ($parent_id != $current_item_id) {
-            db_query("update app_entity_{$current_entity_id} set parent_id={$parent_id} where id={$current_item_id}");
+            if ($parent_id != \K::$fw->current_item_id) {
+                /*db_query(
+                    "update app_entity_{$current_entity_id} set parent_id={$parent_id} where id={$current_item_id}"
+                );*/
+
+                \K::model()->db_update('app_entity_' . (int)\K::$fw->current_entity_id, ['parent_id' => $parent_id], [
+                    'id = ?',
+                    \K::$fw->current_item_id
+                ]);
+            }
+
+            \Helpers\Urls::redirect_to('main/items/info', 'path=' . \K::$fw->app_path);
+        } else {
+            \Helpers\Urls::redirect_to('main/dashboard');
         }
-
-        redirect_to('items/info', 'path=' . $app_path);
     }
 }
--- app/views/main/items/change_parent.php
+++ app/views/main/items/change_parent.php
@@ -3,31 +3,33 @@
 if (!defined('KERUY_CRM')) {
     exit;
 } ?>
-<?php
-echo ajax_modal_template_header(TEXT_CHANGE_PARENT_ITEM) ?>
+<?= \Helpers\App::ajax_modal_template_header(\K::$fw->TEXT_CHANGE_PARENT_ITEM) ?>
 
-
-<?php
-echo form_tag('modal_form', url_for('items/change_parent', 'path=' . $app_path . '&action=change_parent')) ?>
+<?= \Helpers\Html::form_tag(
+    'modal_form',
+    \Helpers\Urls::url_for('main/items/change_parent/change_parent', 'path=' . \K::$fw->app_path)
+) ?>
 <div class="modal-body ajax-modal-width-790">
-
     <div class="dd" id="choices_sort">
-        <?php
-        echo select_entities_tag(
+        <?= \Helpers\Html::select_entities_tag(
             'parent_id',
             [],
             '',
-            ['entities_id' => $current_entity_id, 'is_tree_view' => true, 'parent_item_id' => $parent_entity_item_id]
+            [
+                'entities_id' => \K::$fw->current_entity_id,
+                'is_tree_view' => true,
+                'parent_item_id' => \K::$fw->parent_entity_item_id
+            ]
         ) ?>
     </div>
 </div>
 
-<?php
-echo ajax_modal_template_footer() ?>
+<?= \Helpers\App::ajax_modal_template_footer() ?>
+
 </form>
 
 <script>
     $(function () {
-        $('#modal_form').validate({igonre: ''})
+        $('#modal_form').validate({ignore: ''})
     })
 </script>
