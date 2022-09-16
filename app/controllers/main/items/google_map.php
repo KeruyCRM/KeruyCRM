425a32ba047f00b59fd0bb885a20717dc2176f2b
KeruyCRM <keruy@keruy.com.ua>
2022-09-16 16:48:42 +0300

    google_map.php

--- app/controllers/main/items/google_map.php
+++ app/controllers/main/items/google_map.php
@@ -23,91 +23,147 @@ class Google_map extends \Controller
 
     public function save_value_in()
     {
-        $filed_id = _post::int('filed_id');
-        $distance = (is_numeric($_POST['distance']) ? $_POST['distance'] : 0);
+        if (\K::$fw->VERB == 'POST') {
+            $filed_id = (int)\K::$fw->POST['filed_id'];
+            $distance = (is_numeric(\K::$fw->POST['distance']) ? \K::$fw->POST['distance'] : 0);
 
-        $item_info_query = db_query(
-            "select field_{$filed_id} from app_entity_{$current_entity_id} where id={$current_item_id}"
-        );
-        if ($item_info = db_fetch_array($item_info_query)) {
-            if ($item_info['field_' . $filed_id] != $distance) {
-                db_query(
-                    "update app_entity_{$current_entity_id} set field_{$filed_id}={$distance} where id={$current_item_id}"
+            /*$item_info_query = db_query(
+                "select field_{$filed_id} from app_entity_{\K::$fw->current_entity_id} where id={\K::$fw->current_item_id}"
+            );*/
+
+            $item_info = \K::model()->db_fetch_one('app_entity_' . (int)\K::$fw->current_entity_id, [
+                'id = ?',
+                \K::$fw->current_item_id
+            ], [], 'field_' . $filed_id);
+
+            if ($item_info and $item_info['field_' . $filed_id] != $distance) {
+                /*db_query(
+                    "update app_entity_{\K::$fw->current_entity_id} set field_{$filed_id}={$distance} where id={\K::$fw->current_item_id}"
+                );*/
+
+                \K::model()->db_update(
+                    'app_entity_' . (int)\K::$fw->current_entity_id,
+                    ['field_' . $filed_id => $distance],
+                    [
+                        'id = ?',
+                        \K::$fw->current_item_id
+                    ]
                 );
 
                 echo 'UPDATED';
             }
+        } else {
+            \Helpers\Urls::redirect_to('main/dashboard');
         }
     }
 
     public function update_latlng()
     {
-        $filed_id = _post::int('filed_id');
+        if (\K::$fw->VERB == 'POST') {
+            $filed_id = (int)\K::$fw->POST['filed_id'];
 
-        $item_info_query = db_query(
-            "select field_{$filed_id} from app_entity_{$current_entity_id} where id={$current_item_id}"
-        );
-        if ($item_info = db_fetch_array($item_info_query)) {
-            //get current address
-            if (strlen($item_info['field_' . $filed_id])) {
+            /*$item_info_query = db_query(
+                "select field_{$filed_id} from app_entity_{\K::$fw->current_entity_id} where id={\K::$fw->current_item_id}"
+            );*/
+
+            $item_info = \K::model()->db_fetch_one('app_entity_' . (int)\K::$fw->current_entity_id, [
+                'id = ?',
+                \K::$fw->current_item_id
+            ], [], 'field_' . $filed_id);
+
+            if ($item_info and strlen($item_info['field_' . $filed_id])) {
+                //get current address
                 $value = explode("\t", $item_info['field_' . $filed_id]);
-
-                //print_r($value);
 
                 $current_address = $value[2];
 
-                $value = $_POST['lat'] . "\t" . $_POST['lng'] . "\t" . $current_address;
+                $value = \K::$fw->POST['lat'] . "\t" . \K::$fw->POST['lng'] . "\t" . $current_address;
 
-                db_query(
-                    "update app_entity_{$current_entity_id} set field_{$filed_id}='" . db_input(
+                /*db_query(
+                    "update app_entity_{\K::$fw->current_entity_id} set field_{$filed_id}='" . db_input(
                         $value
-                    ) . "' where id='" . db_input($current_item_id) . "'"
+                    ) . "' where id='" . db_input(\K::$fw->current_item_id) . "'"
+                );*/
+
+                \K::model()->db_update(
+                    'app_entity_' . (int)\K::$fw->current_entity_id,
+                    ['field_' . $filed_id => $value],
+                    [
+                        'id = ?',
+                        \K::$fw->current_item_id
+                    ]
                 );
             }
+        } else {
+            \Helpers\Urls::redirect_to('main/dashboard');
         }
     }
 
     public function update_latlng_multiple()
     {
-        $filed_id = _post::int('filed_id');
-        $address_key = _post::int('address_key');
+        if (\K::$fw->VERB == 'POST') {
+            $filed_id = (int)\K::$fw->POST['filed_id'];
+            $address_key = \K::$fw->POST['address_key'];
 
-        $item_info_query = db_query(
-            "select field_{$filed_id} from app_entity_{$current_entity_id} where id={$current_item_id}"
-        );
-        if ($item_info = db_fetch_array($item_info_query)) {
-            //get current address
-            if (strlen($item_info['field_' . $filed_id])) {
+            /*$item_info_query = db_query(
+                "select field_{$filed_id} from app_entity_{\K::$fw->current_entity_id} where id={\K::$fw->current_item_id}"
+            );*/
+
+            $item_info = \K::model()->db_fetch_one('app_entity_' . (int)\K::$fw->current_entity_id, [
+                'id = ?',
+                \K::$fw->current_item_id
+            ], [], 'field_' . $filed_id);
+
+            if ($item_info and strlen($item_info['field_' . $filed_id])) {
+                //get current address
+
                 $item_address_array = preg_split("/\\r\\n|\\r|\\n/", $item_info['field_' . $filed_id]);
 
                 if (isset($item_address_array[$address_key])) {
                     if (strlen($item_address_array[$address_key])) {
                         $value = explode("\t", $item_address_array[$address_key]);
 
-                        $item_address_array[$address_key] = $_POST['lat'] . "\t" . $_POST['lng'] . "\t" . $value[2];
+                        $item_address_array[$address_key] = \K::$fw->POST['lat'] . "\t" . \K::$fw->POST['lng'] . "\t" . $value[2];
                     }
                 }
 
-                db_query(
-                    "update app_entity_{$current_entity_id} set field_{$filed_id}='" . db_input(
+                /*db_query(
+                    "update app_entity_{\K::$fw->current_entity_id} set field_{$filed_id}='" . db_input(
                         implode("\n", $item_address_array)
-                    ) . "' where id='" . db_input($current_item_id) . "'"
+                    ) . "' where id='" . db_input(\K::$fw->current_item_id) . "'"
+                );*/
+
+                \K::model()->db_update(
+                    'app_entity_' . (int)\K::$fw->current_entity_id,
+                    ['field_' . $filed_id => implode("\n", $item_address_array)],
+                    [
+                        'id = ?',
+                        \K::$fw->current_item_id
+                    ]
                 );
             }
+        } else {
+            \Helpers\Urls::redirect_to('main/dashboard');
         }
     }
 
     public function update_latlng_directions()
     {
-        $filed_id = _post::int('filed_id');
-        $lat = $_POST['lat'];
-        $lng = $_POST['lng'];
+        if (\K::$fw->VERB == 'POST') {
+            $filed_id = (int)\K::$fw->POST['filed_id'];
+            $lat = \K::$fw->POST['lat'];
+            $lng = \K::$fw->POST['lng'];
 
-        $item_info_query = db_query(
-            "select field_{$filed_id} from app_entity_{$current_entity_id} where id={$current_item_id}"
-        );
-        if ($item_info = db_fetch_array($item_info_query)) {
-            if (strlen($item_info['field_' . $filed_id])) {
+            /*$item_info_query = db_query(
+                "select field_{$filed_id} from app_entity_{\K::$fw->current_entity_id} where id={\K::$fw->current_item_id}"
+            );*/
+
+            $item_info = \K::model()->db_fetch_one('app_entity_' . \K::$fw->current_entity_id, [
+                'id = ?',
+                \K::$fw->current_item_id
+            ], [], 'field_' . $filed_id);
+
+            if ($item_info and strlen($item_info['field_' . $filed_id])) {
                 $item_address_array = preg_split("/\\r\\n|\\r|\\n/", $item_info['field_' . $filed_id]);
 
                 foreach ($lat as $address_key => $lat_value) {
@@ -115,19 +171,28 @@ class Google_map extends \Controller
                         if (strlen($item_address_array[$address_key])) {
                             $value = explode("\t", $item_address_array[$address_key]);
 
-                            $item_address_array[$address_key] = $lat[$address_key] . "\t" . $lng[$address_key] . "\t" . $value[2];
+                            $item_address_array[$address_key] = $lat_value . "\t" . $lng[$address_key] . "\t" . $value[2];
                         }
                     }
                 }
 
-                //print_r($item_address_array);
-
-                db_query(
-                    "update app_entity_{$current_entity_id} set field_{$filed_id}='" . db_input(
+                /*db_query(
+                    "update app_entity_{\K::$fw->current_entity_id} set field_{$filed_id}='" . db_input(
                         implode("\n", $item_address_array)
-                    ) . "' where id='" . db_input($current_item_id) . "'"
+                    ) . "' where id='" . db_input(\K::$fw->current_item_id) . "'"
+                );*/
+
+                \K::model()->db_update(
+                    'app_entity_' . (int)\K::$fw->current_entity_id,
+                    ['field_' . $filed_id => implode("\n", $item_address_array)],
+                    [
+                        'id = ?',
+                        \K::$fw->current_item_id
+                    ]
                 );
             }
+        } else {
+            \Helpers\Urls::redirect_to('main/dashboard');
         }
     }
 }
