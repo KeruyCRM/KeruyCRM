2f30573251621d4d6124d0c8d6680af287196fe0
KeruyCRM <keruy@keruy.com.ua>
2022-09-26 21:16:07 +0300

    import_bind.php

---
+++ app/controllers/main/items/import_bind.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * KeruyCRM (c)
+ * https://keruy.com.ua
+ */
+
+namespace Controllers\Main\Items;
+
+class Import_bind extends \Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        \K::security()->checkCsrfToken();
+
+        \Controllers\Main\Items\_Module::top();
+    }
+
+    public function index()
+    {
+        \K::$fw->subTemplate = \K::$fw->pathSubTemplate . 'import_bind.php';
+
+        echo \K::view()->render(\K::$fw->app_layout);
+    }
+}
